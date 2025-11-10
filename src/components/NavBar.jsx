e97f3319e85c87e9bd03f2f5e9cc122e71b7f8c8
moises montt <moises.montt287@gmail.com>
2025-11-10 18:24:07 -0300

    correccion de las categorias del navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,12 +10,27 @@ function Navbar() {
           <h1>Axiom Play</h1>
         </Link>
         
-        <ul className="navbar-categories">
-          <li><Link to="/category/smartphones">Smartphones</Link></li>
-          <li><Link to="/category/laptops">Laptops</Link></li>
-        </ul>
-        
-        <CartWidget />
+        <div className="navbar-widgets">
+          <div className="dropdown">
+            <button 
+              className="navbar-toggler dropdown-toggle" 
+              type="button" 
+              data-bs-toggle="dropdown"
+            >
+              <i className="fa-solid fa-bars"></i>
+            </button>
+            <ul className="dropdown-menu dropdown-menu-end">
+              <li><Link className="dropdown-item" to="/category/smartphones">Smartphones</Link></li>
+              <li><Link className="dropdown-item" to="/category/laptops">Laptops</Link></li>
+              <li><Link className="dropdown-item" to="/category/fragrances">Fragrances</Link></li>
+              <li><Link className="dropdown-item" to="/category/beauty">Beauty</Link></li>
+              <li><Link className="dropdown-item" to="/category/furniture">Furniture</Link></li>
+              <li><Link className="dropdown-item" to="/category/groceries">Groceries</Link></li>
+            </ul>
+          </div>
+          
+          <CartWidget />
+        </div>
       </div>
     </nav>
   )
